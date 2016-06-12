84488ece382de60b0224151009c595758f407f3e
Guido Nicolás Quadrini <guidoquadrini@hotmail.com>
2016-06-12 09:03:13 -0300

    Identificacion de version

--- QHosting/My Project/AssemblyInfo.vb
+++ QHosting/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
-<Assembly: Guid("bc061a8b-22d8-4837-a080-f71d86b4fa03")> 
+<Assembly: Guid("bc061a8b-22d8-4837-a080-f71d86b4fa03")>
 
 ' La información de versión de un ensamblado consta de los cuatro valores siguientes:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' mediante el asterisco ('*'), como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.1606.12.1")>
+<Assembly: AssemblyFileVersion("1.1606.12.1")>
